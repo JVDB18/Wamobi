fd08cd075d75b3396cf77a3371d32672c8d13724
badou <badou@pop-os.localdomain>
2019-03-23 15:36:45 +0100

    fix coordonnées

--- src/client/Pages/Map.js
+++ src/client/Pages/Map.js
@@ -1,28 +1,40 @@
 import React from "react";
 import L from "leaflet";
 
-let options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-};
-
-const success = pos => {
-    console.log(`Latitude: ${pos.coords.latitude}`);
-    console.log(`Longitude: ${pos.coords.longitude}`);
-};
-
-const error = err => {
-    console.warn(`ERREUR (${err.code}): ${err.message}`);
-};
-
-navigator.geolocation.watchPosition(success, error, options);
-
 export default class Map extends React.Component {
+    state = {
+        center_lat: "",
+        center_lng: "",
+    };
+
+    componentWillMount() {
+        let options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        };
+
+        const success = pos => {
+            console.log(`Latitude: ${pos.coords.latitude}`);
+            console.log(`Longitude: ${pos.coords.longitude}`);
+
+            this.setState({
+                center_lat: pos.coords.latitude,
+                center_lng: pos.coords.longitude,
+            });
+        };
+
+        const error = err => {
+            console.warn(`ERREUR (${err.code}): ${err.message}`);
+        };
+
+        navigator.geolocation.watchPosition(success, error, options);
+    }
+
     componentDidMount() {
         // create map
         this.map = L.map("map", {
-            center: [49.8419, 24.0315],
+            center: [this.state.center_lat, this.state.center_lng],
             zoom: 16,
             layers: [
                 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
